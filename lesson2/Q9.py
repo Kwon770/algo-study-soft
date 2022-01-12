bdc5663d299dc095ee3a9fcfc91dfcbd2667082e
Kwon770 <sckwon770@gmail.com>
2022-01-13 03:22:05 +0900

    Fail lesson2 - Q9

---
+++ lesson2/Q9.py
@@ -0,0 +1,38 @@
+def solution(s):
+    ans = 987654321
+    for size in range(1, len(s)//2 + 1):
+        
+        localAns = 0
+        idx = 0
+        isEndStep = False
+        while idx < len(s):
+            
+            zipStep = 1
+            isZipped = True
+            while isZipped:
+                if idx + (zipStep+1) * size > len(s):
+                    isZipped = False
+                    isEndStep = True
+                    break;
+                    
+                if s[idx:idx + size] != s[idx + zipStep * size:idx + (zipStep+1) * size]:
+                    isZipped = False
+                    break;
+                    
+                zipStep += 1
+                
+            if zipStep > 1:
+                localAns += 1+size
+                idx += zipStep * size
+            else:
+                if isEndStep:
+                    localAns += len(s[idx:])
+                    break
+                else:
+                    localAns += size
+                    idx += size
+                    
+        if ans > localAns:
+            ans = localAns
+
+    return ans
